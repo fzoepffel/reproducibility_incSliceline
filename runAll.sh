7470e5d439808eae1518ea9a637cb6fa1f0ade7a
Frederic Zoepffel <f.zoepffel@gmail.com>
2024-08-09 15:35:41 +0200

    first experiment including plotting

--- runAll.sh
+++ runAll.sh
@@ -10,6 +10,4 @@ rm -R plots/*;
 ./run3DownloadData.sh;
 ./run4PrepareLocalData.sh; # on scale-up node
 ./run5LocalExperiments.sh; # on scale-up node 
-./run6PrepareDistData.sh;  # on scale-out cluster
-./run7DistExperiments.sh;  # on scale-out cluster
 ./run8PlotResults.sh;
